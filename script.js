f81a63cee042aba8f26177863633404254e1f70d
ajayhandlooms <mintusingla50541@gmail.com>
2025-10-27 22:10:03 +0530

    Main Files
    Jai Bajrangbali

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for quick links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+
